c67562618e31dc739117d243e25c279eaf17fbdd
apoorva87 <apoorvakarnik@gmail.com>
2016-11-27 00:00:09 -0800

    Removes method 2 for back ground filteration. Saves video when motion is seen.

--- BGMOG2_example.py
+++ BGMOG2_example.py
@@ -1,14 +1,13 @@
 import numpy as np
 import cv2
-
-cap = cv2.VideoCapture('output.avi')
+# Simple video read and display
+# with background substraction
+cap  = cv2.VideoCapture('video.avi')
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
     ret, frame = cap.read()
-
     fgmask = fgbg.apply(frame)
-
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
--- motion.py
+++ motion.py
@@ -3,34 +3,36 @@ import cv2
 import imutils
 import datetime
 
-camera    = cv2.VideoCapture(0)
-#camera     = cv2.VideoCapture('Output.avi')
+camera     = cv2.VideoCapture(0)
+#camera    = cv2.VideoCapture('Output.avi')
 fgbg       = cv2.createBackgroundSubtractorMOG2()
+fourcc     = cv2.VideoWriter_fourcc(*'XVID')
+out        = cv2.VideoWriter('video.avi',fourcc, 20.0, (640,480))
 firstFrame = None
 while True:
     ret, frame = camera.read()
     text       = 'Unoccupied'
     if not ret:
         break;
-    frame      = imutils.resize(frame,width=500)
-    gray       = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray       = cv2.GaussianBlur(gray,(21,21),0)
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    #1 Method 1
-    fgmask     = fgbg.apply(frame)
-    #2 Method 2
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh     = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    thresh     = cv2.dilate(thresh, None, iterations=2)
-    (_,cnts1, _) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    (_,cnts2, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.imshow('Baldy stuff', thresh)
-    cv2.imshow('BG stuff', fgmask)
+    ##1 Method 1
+    fgmask = fgbg.apply(frame)
+    ##2 Method 2
+    #frame      = imutils.resize(frame,width=500)
+    #gray       = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray       = cv2.GaussianBlur(gray,(21,21),0)
+    #if firstFrame is None:
+    #    firstFrame = gray
+    #    continue
+    #frameDelta = cv2.absdiff(firstFrame, gray)
+    #thresh     = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    #thresh     = cv2.dilate(thresh, None, iterations=2)
+    #(_,cnts2, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.imshow('Baldy stuff', thresh)
+    #cv2.imshow('BG stuff', fgmask)
+    (_, cnts1, _) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in cnts1:
-        print("area ",cv2.contourArea(c))
-        if cv2.contourArea(c) < 4000:
+        #print("area ",cv2.contourArea(c))
+        if cv2.contourArea(c) < 1000:
             continue
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
@@ -43,8 +45,10 @@ while True:
 
     # show the frame and record if the user presses a key
     cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
+    if text == 'Occupied':
+      out.write(frame)
+    #cv2.imshow("Thresh", thresh)
+    #cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
 
     # if the `q` key is pressed, break from the lop
@@ -53,4 +57,5 @@ while True:
 
     # cleanup the camera and close any open windows
 camera.release()
+out.release()
 cv2.destroyAllWindows()
